9930fd51d097a42d105bbae16985963bcfd7e0d6
RafaelNagai <rafakenji23@gmail.com>
2025-10-05 21:11:31 -0700

    feat: add dotenv support for environment variables and configure API URL

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:arcadia_rpg/app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   runApp(ProviderScope(child: const ArcadiaApp()));
 }
--- lib/main_web.dart
+++ lib/main_web.dart
@@ -1,9 +1,12 @@
 import 'package:arcadia_rpg/app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: "env/.env");
   setUrlStrategy(PathUrlStrategy());
   runApp(ProviderScope(child: const ArcadiaApp()));
 }
